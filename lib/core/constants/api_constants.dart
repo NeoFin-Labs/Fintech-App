c5c63b3ed601610ed48935f142482a7e09c35e56
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-11-22 03:17:37 +0200

    feat: add ApiConstants class for API configuration including base URL and timeouts

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,11 @@
+
+class ApiConstants {
+  static const String baseUrl = 'https://api.coingecko.com/api/v3/';
+  static const String apiKey ='1234567890';
+  static const int connectionTimeout = 10000;
+  static const int receiveTimeout = 10000;
+  static const int sendTimeout = 10000;
+
+}
+class ApiEndpoints {
+}
