5aaad8068fc361b7744d249e0108213a47da26b2
Mostafa Ajep <mostafaajep@gmail.com>
2025-11-25 12:56:57 +0200

    Initial commit from feature/home

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,13 @@
+
+import 'package:fintech_app/core/constants/env.dart';
+
+class ApiConstants {
+  static const String baseUrl = Env.baseUrl;
+  static final String apiKey = Env.apiKey;
+  static const int connectionTimeout = 10000;
+  static const int receiveTimeout = 10000;
+  static const int sendTimeout = 10000;
+
+}
+class ApiEndpoints {
+}
---
+++ lib/core/constants/pagination_constants.dart
@@ -0,0 +1,4 @@
+class PaginationConstants {
+  static const int pageSize = 10;
+  static int pageNumber = 1;
+}
