4a7395cc20369c9400b2c65094906493dc29abe5
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-11-26 15:05:36 +0200

    feat: Initialize the Flutter application with core theming, routing, and main entry point.

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(
+        color: Colors.black,
+      ),
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  );
+}
---
+++ lib/fintech_app.dart
@@ -0,0 +1,30 @@
+import 'package:fintech_app/core/routes/app_routes.dart';
+import 'package:fintech_app/core/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FintechApp extends StatelessWidget {
+  const FintechApp({ super.key,
+    required this.appRoutes,
+  });
+  final AppRoutes appRoutes;
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Fintech App',
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: appRoutes.generateRoute,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: ThemeMode.system,
+        );
+      },
+    );
+  }
+}
