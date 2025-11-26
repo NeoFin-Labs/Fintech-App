c9ed79b5172c788f34129b39615ed2c9dbb72a6e
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-11-27 00:54:22 +0200

    feat: add `NavigationExtension` on `BuildContext` for simplified navigation.

---
+++ lib/core/routes/Navigation_extension.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+extension NavigationExtension on BuildContext {
+  void pushNamed(String routeName, {Object? arguments}) {
+    Navigator.pushNamed(this, routeName, arguments: arguments);
+  }
+
+  void pushReplacementNamed(String routeName, {Object? arguments}) {
+    Navigator.pushReplacementNamed(this, routeName, arguments: arguments);
+  }
+
+  void pop() {
+    Navigator.pop(this);
+  }
+}
