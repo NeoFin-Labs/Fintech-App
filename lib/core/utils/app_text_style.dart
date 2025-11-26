8f9a4884d65c0e18cd56f2d4bc436cd11baa4c05
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-11-27 00:58:07 +0200

    feat: add AppTextStyle utility class for consistent text styling

--- lib/core/utils/app_text_style.dart
+++ lib/core/utils/app_text_style.dart
@@ -6,55 +6,66 @@ class AppTextStyle {
   static TextStyle font32Bold = TextStyle(
     fontSize: 32.sp,
     fontWeight: FontWeight.w700,
+    height: 1.5,
   );
 
   static TextStyle font24Bold = TextStyle(
     fontSize: 24.sp,
     fontWeight: FontWeight.w700,
+    height: 1.5,
   );
 
   static TextStyle font20Medium = TextStyle(
     fontSize: 20.sp,
     fontWeight: FontWeight.w500,
+    height: 1.5,
   );
 
   static TextStyle font18Bold = TextStyle(
     fontSize: 18.sp,
     fontWeight: FontWeight.w700,
+    height: 1.5,
   );
 
   static TextStyle font16Bold = TextStyle(
     fontSize: 16.sp,
     fontWeight: FontWeight.w700,
+    height: 1.5,
   );
 
   static TextStyle font16Medium = TextStyle(
     fontSize: 16.sp,
     fontWeight: FontWeight.w500,
+    height: 1.5,
   );
 
   static TextStyle font16Regular = TextStyle(
     fontSize: 16.sp,
     fontWeight: FontWeight.w400,
+    height: 1.5,
   );
 
   static TextStyle font14Bold = TextStyle(
     fontSize: 14.sp,
     fontWeight: FontWeight.w700,
+    height: 1.5,
   );
 
   static TextStyle font14Regular = TextStyle(
     fontSize: 14.sp,
     fontWeight: FontWeight.w400,
+    height: 1.5,
   );
 
   static TextStyle font12SemiBold = TextStyle(
     fontSize: 12.sp,
     fontWeight: FontWeight.w600,
+    height: 1.5,
   );
 
   static TextStyle font12Regular = TextStyle(
     fontSize: 12.sp,
     fontWeight: FontWeight.w400,
+    height: 1.5,
   );
 }
