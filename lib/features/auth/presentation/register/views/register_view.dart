0fee4e5360a3f123b0eb26b4c90055ffd2daceb6
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-12-01 06:55:30 +0200

    feat: create RegisterView to assemble registration UI with header, form, and footer components

---
+++ lib/features/auth/presentation/register/views/register_view.dart
@@ -0,0 +1,45 @@
+import 'package:fintech_app/core/helper/spacing.dart';
+import 'package:fintech_app/core/routes/navigation_extension.dart';
+import 'package:fintech_app/core/routes/routes.dart';
+import 'package:fintech_app/features/auth/presentation/common/widgets/auth_background.dart';
+import 'package:fintech_app/features/auth/presentation/register/views/register_form.dart';
+import 'package:fintech_app/features/auth/presentation/register/widgets/register_footer.dart';
+import 'package:fintech_app/features/auth/presentation/register/widgets/register_header.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AuthBackground(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            VerticalSpace(40),
+
+            // Header
+            const RegisterHeader(),
+            VerticalSpace(32),
+
+            // Register Form
+            const RegisterForm(),
+            VerticalSpace(24),
+
+            // Footer
+            RegisterFooter(
+              onLoginTap: () {
+                context.pushReplacementNamed(Routes.login);
+              },
+            ),
+
+            VerticalSpace(24),
+          ],
+        ),
+      ),
+    );
+  }
+}
