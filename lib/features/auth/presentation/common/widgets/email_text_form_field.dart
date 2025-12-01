6469dc8592d014a801bbff28729520d2dd62e39f
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-12-01 02:31:20 +0200

    feat: update EmailTextFormField to accept a TextEditingController and add email validation regex for improved input handling

--- lib/features/auth/presentation/common/widgets/email_text_form_field.dart
+++ lib/features/auth/presentation/common/widgets/email_text_form_field.dart
@@ -2,7 +2,9 @@ import 'package:fintech_app/core/widgets/app_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class EmailTextFormField extends StatelessWidget {
-  const EmailTextFormField({super.key});
+  const EmailTextFormField({super.key, required this.controller});
+
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +12,15 @@ class EmailTextFormField extends StatelessWidget {
       hintText: 'E-mail ID',
       prefixIcon: Icons.email_outlined,
       labelText: 'E-mail ID',
-      controller: TextEditingController(),
+      controller: controller,
       validator: (value) {
         if (value == null || value.isEmpty) {
           return 'Please enter your email address';
+        }
+        // Regex for email validation
+        final emailRegex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+        if (!emailRegex.hasMatch(value)) {
+          return 'Please enter a valid email address';
         }
         return null;
       },
