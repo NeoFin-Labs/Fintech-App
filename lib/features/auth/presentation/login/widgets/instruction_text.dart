cd2c832454ae523b7a405f91cb87abdc52ef6553
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-12-02 14:16:34 +0200

    feat: enhance BiometricLoginSection and InstructionText with responsive design and customizable properties

--- lib/features/auth/presentation/login/widgets/instruction_text.dart
+++ lib/features/auth/presentation/login/widgets/instruction_text.dart
@@ -4,25 +4,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class InstructionText extends StatelessWidget {
-  const InstructionText({super.key});
+  final String text;
+  final Color? textColor;
+  final bool withShadow;
+
+  const InstructionText({
+    super.key,
+    this.text = 'Please wait until your scanning is complete',
+    this.textColor,
+    this.withShadow = true,
+  });
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).appColors;
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40.w),
-      child: Text(
-        'Please wait until your scanning is complete',
-        textAlign: TextAlign.center,
-        style: AppTextStyle.font18SemiBold.copyWith(
-          color: colors.white,
-          shadows: [
-            Shadow(
-              color: colors.black.withValues(alpha: 0.3),
-              blurRadius: 10.r,
-            ),
-          ],
-        ),
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: AppTextStyle.font18SemiBold.copyWith(
+        color: textColor ?? colors.white,
+        shadows: withShadow
+            ? [
+                Shadow(
+                  color: colors.black.withValues(alpha: 0.3),
+                  blurRadius: 10.r,
+                ),
+              ]
+            : null,
       ),
     );
   }
