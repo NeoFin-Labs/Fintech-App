e84df10c89725840b50f92121c32041c67a93daa
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-12-06 11:34:46 +0200

    feat: initialize dependency injection container in main function

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:fintech_app/fintech_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'package:fintech_app/core/di/injection_container.dart' as di;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // ensureScreenSize() must be called before runApp() to fix the issue of the text not being displayed in release mode
   await ScreenUtil.ensureScreenSize();
+  await di.init();
   runApp(const FintechApp(appRoutes: AppRoutes.instance));
 }
